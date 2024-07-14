1c9e8fb6173761005a96c9bda7c281a7cb8ec217
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-07-14 17:46:11 +0530

    Added HomePage and Dark Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+import { ThemeProvider } from "./components/theme-provider";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage></HomePage>,
+    },
+    {
+      path: "/chat",
+      element: <ChatPage></ChatPage>,
+    },
+  ]);
+
   return (
     <>
-      <HomePage></HomePage>
+      {" "}
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,18 @@
-import { Button } from "../components/ui/button"
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/ui/button";
 
 const HomePage = () => {
-  return (
-    <div>
-        <Button>Click me</Button>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default HomePage
+  const handleNavigate = () => {
+    navigate("/chat");
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full h-screen dark:bg-dark ">
+      <Button onClick={handleNavigate}>Start the Chat</Button>
+    </div>
+  );
+};
+
+export default HomePage;
